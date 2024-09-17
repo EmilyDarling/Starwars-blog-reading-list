6d5317c8d91a7c6fa6eb7f6e8aff45798a1fa928
Angelina <angelaaquin13@gmail.com>
2024-09-17 01:34:17 +0000

    fixing fetch names

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1 mx-5"><img src="https://pngimg.com/d/star_wars_logo_PNG34.png" className="mx-3" style={{width: 50}}/></span>
+				
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="ml-auto mx-5 ">
+			<div className="dropdown">
+			<button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+				Favorites
+			</button>
+			<ul className="dropdown-menu">
+				<li><a className="dropdown-item" href="#">Action</a></li>
+				<li><a className="dropdown-item" href="#">Another action</a></li>
+				<li><a className="dropdown-item" href="#">Something else here</a></li>
+			</ul>
+			</div>
 			</div>
 		</nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useState , useEffect} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
+export const Home = () => {
+
+	const [character, setCharacter] = useState("");
+	const [characterBank, setCharacterBank] = useState([]);
+
+	const getCharacterBank = () => {
+		fetch('https://www.swapi.tech/api/people')
+		.then(response => {
+			if (!response.ok) {
+				throw Error(response.statusText);
+			}
+			return response.json();
+		})
+
+		.then(responseAsJson => {
+			console.log(responseAsJson);
+			setCharacterBank(responseAsJson.results);			
+		})
+
+		.catch(error => {
+			console.log('Looks like there was a problem: \n', error);
+		});
+	  };
+
+
+	  useEffect(() => {
+		getCharacterBank();	
+	}, []);
+
+	return(
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>Characters</h1>
+		<div id="characterCards">
+			
+		{characterBank.map((characterStats,index) =>
+					<div className="card" style={{width:300}}>
+					<svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" 
+				   aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+					   <div className="card-body">
+						   <h5 className="card-title">{characterStats.name}</h5>
+						   <p className="card-text">Gender: {characterStats.url}</p>
+						   <p className="card-text">Hair Color: {characterStats.hair_color}</p>
+						   <p className="card-text">Eye Color: {characterStats.eye_color}</p>
+						   <a href="#" className="btn btn-primary me-2">Learn More!</a>
+						   <a href="#" className="btn btn-primary ms-2"> heart </a>
+					   </div>
+				   </div>
+				   )}
+					)
+				}
+				 
+		</div>
+		
 	</div>
 );
+}
